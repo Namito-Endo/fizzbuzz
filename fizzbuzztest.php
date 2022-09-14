98270f67f38d1203a0fe54f1c2ad5c7156e08471
Namito-Endo <namtio.end0113@gmail.com>
2022-09-15 01:35:27 +0900

    fizzbuzz課題

---
+++ fizzbuzztest.php
@@ -0,0 +1,14 @@
+<?php
+for($x=1; $x<=100; $x++){
+	if($x % 15 === 0){
+	echo "fizzbuzz" . PHP_EOL;
+	}elseif($x % 3 === 0){
+	echo "fizz" . PHP_EOL;
+	}elseif($x %5 === 0){
+	echo "buzz" . PHP_EOL;
+	}else{
+	echo "$x" . PHP_EOL;
+	}
+}
+?>
+

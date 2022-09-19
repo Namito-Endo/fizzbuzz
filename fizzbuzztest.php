7dbecf804b243b31ef1deef9bb6ee64f9ebd6fce
Namito-Endo <namtio.end0113@gmail.com>
2022-09-19 15:26:40 +0900

    indent fixed

--- fizzbuzztest.php
+++ fizzbuzztest.php
@@ -1,14 +1,13 @@
 <?php
-//why can't push????
+//インデントの修正
 for($x=1; $x<=100; $x++){
 	if($x % 15 === 0){
-	echo "fizzbuzz" . PHP_EOL;
+		echo "fizzbuzz" . PHP_EOL;
 	}elseif($x % 3 === 0){
-	echo "fizz" . PHP_EOL;
+		echo "fizz" . PHP_EOL;
 	}elseif($x %5 === 0){
-	echo "buzz" . PHP_EOL;
-	}else{
-	echo "$x" . PHP_EOL;
+		echo "buzz" . PHP_EOL;
+	}else{echo "$x" . PHP_EOL;
 	}
 }
 ?>
